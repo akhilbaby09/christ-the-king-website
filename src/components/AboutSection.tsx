25ec5a1348a69c2560a28cd860802da08df61045
akhilbaby09 <akhilbaby09@gmail.com>
2025-12-09 14:43:14 -0600

    Update AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -20,16 +20,20 @@ export const AboutSection = () => {
           {/* Text Content */}
           <div className="space-y-6">
             <p className="font-body text-foreground leading-relaxed">
-              Our doors—and our hearts—are open to every soul longing to experience the grace 
-              of Jesus Christ. We invite you to come with an open heart and allow His presence 
-              to fill your life with peace and purpose.
+              Kerala is a state on the southwestern Malabar Coast of India. It was formed on 1 November 1956, following the passage of the States Reorganization Act, by combining Malayalam-speaking regions of the erstwhile states of Travancore-Cochin and Madras. Spread over 38,863 km2 (15,005 sq mi), Kerala is the twenty-first largest Indian state by area. It is bordered by Karnataka to the north and northeast, Tamil Nadu to the east and south, and the Lakshadweep Sea to the west. With 33,387,677 inhabitants as per the 2011 Census, Kerala is the thirteenth-largest Indian state by population. It is divided into 14 districts with the capital being Thiruvananthapuram. Malayalam is the most widely spoken language and is also the official language of the state.
             </p>
             <p className="font-body text-foreground leading-relaxed">
-              We are a diverse parish family, with members of all ages and backgrounds joining 
-              together to worship, celebrate, and serve God's people. If you wish to learn more 
-              or join us for a service, we would love to connect with you.
+             The Syro-Malabar Catholic Eparchy of Mississauga is an eparchy for all Syro-Malabar Catholics in Canada. It comprises 16 churches and 39 missions. Most churches are found in the province of Ontario.
+The Cathedral for the Syro Malabars in Canada is the St Alphonsa Cathedral of Mississauga. The Cathedral was purchased in the 2010s. The first Church purchased was St Thomas Syro Malabar Catholic Church Scarborough, which is currently the second largest Syro Malabar Church in Canada. All the parishes and Missions have significant and growing populations. The Syro-Malabar Catholic Church is a Major Archiepiscopal Church in full communion with the Roman Catholic Church.
             </p>
-            <blockquote className="border-l-4 border-gold pl-6 italic text-muted-foreground">
+<p className="font-body text-foreground leading-relaxed">
+            The first Syro-Malabar holy mass in Saskatoon was celebrated in June 2009 at St. Marys’ Church, Saskatoon.
+The first family prayer meeting was also started in August 2013 at Thomas Valakkamattathil’s house. Since then, the family prayer meeting was rotated through other families every month.
+In Sep 2015 we started our kids’ catechism once in every month by Smino Varghese and Binu Jacob.
+In September 2015 Exarchate for Syro-Malabar in Canada was formed and the Bishop of the Exarchate Bishop Mar Jose Kalluvelil expressed his desire to visit Saskatoon. The Bishop’s visit on 29th Nov 2015 was celebrated with a holy mass by Bishop and Fr. Raphael at St. Michaels church.
+During this visit Bishop officially declared Saskatoon as a Syro-Malabar parish and named Christ The King. Bishop also insists us to celebrate two holy mass every month, which was celebrated on Second and Fourth Sunday
+           </p>
+  <blockquote className="border-l-4 border-gold pl-6 italic text-muted-foreground">
               "We're always happy to see you and warmly welcome visitors."
             </blockquote>
           </div>
