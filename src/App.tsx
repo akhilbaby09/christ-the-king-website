f539c824738cb84b939bb7bff2925526b8c333d8
akhilbaby09 <akhilbaby09@gmail.com>
2025-12-09 20:10:09 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Gallery from "./src/components/gallery";  // <-- add this
+import Gallery from "./src/pages/gallery";  // <-- add this
 
 const queryClient = new QueryClient();
 
